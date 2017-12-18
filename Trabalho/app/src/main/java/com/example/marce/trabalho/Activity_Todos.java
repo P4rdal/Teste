81d1bc7d9767b8fed4dca87c8118c451745bfa80
P4rdal <marcelodeiro@hotmail.com>
2017-12-17 23:12:55 -0300

    feito

--- Trabalho/app/src/main/java/com/example/marce/trabalho/Activity_Todos.java
+++ Trabalho/app/src/main/java/com/example/marce/trabalho/Activity_Todos.java
@@ -1,11 +1,17 @@
 package com.example.marce.trabalho;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class Activity_Todos extends AppCompatActivity {
 
@@ -18,6 +24,7 @@ public class Activity_Todos extends AppCompatActivity {
         setContentView(R.layout.activity_todos);
 
         /**SOMENTE PARA TESTE**/
+
 
 
 
